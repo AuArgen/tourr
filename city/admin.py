882a6a610713b182ee0ff19fa6b78a33a723aba1
AuArgen <argenazizbekuulu3@gmail.com>
2025-03-11 22:34:08 +0600

    added new url for home page

--- city/admin.py
+++ city/admin.py
@@ -6,5 +6,6 @@ from city.models import City
 # Register your models here.
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
-    list_display = ('title', 'image', 'count_tours')
+    list_display = ('title', 'image', 'count_tours', 'is_show')
     search_fields = ('title', )
+    list_filter = ('is_show', )
--- city/views.py
+++ city/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from city.models import City
+
+
 # Create your views here.
+def home(request):
+    cities = City.objects.filter(is_show=True).order_by('id')
+    context = {
+        'cities': cities
+    }
+    return render(request, 'index.html', context)
